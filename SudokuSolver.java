ceb7ade71925a74d1a0f42a59d8eefce5ad06040
dubbakaramya1310 <139442224+dubbakaramya1310@users.noreply.github.com>
2024-06-30 18:40:11 +0530

    Add files via upload

---
+++ SudokuSolver.java
@@ -0,0 +1,64 @@
+public class SudokuSolver {
+    private static final int SIZE = 9;
+    private static final int SUBGRID_SIZE = 3;
+
+    public static void main(String[] args) {
+        int[][] board = {
+            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            {0, 0, 0, 0, 8, 0, 0, 7, 9}
+        };
+
+        if (solveSudoku(board)) {
+            System.out.println("Sudoku puzzle solved successfully:");
+            printBoard(board);
+        } else {
+            System.out.println("No solution exists for the given Sudoku puzzle.");
+        }
+    }
+
+    private static boolean solveSudoku(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                if (board[row][col] == 0) {
+                    for (int num = 1; num <= SIZE; num++) {
+                        if (isSafe(board, row, col, num)) {
+                            board[row][col] = num;
+                            if (solveSudoku(board)) {
+                                return true;
+                            }
+                            board[row][col] = 0;
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private static boolean isSafe(int[][] board, int row, int col, int num) {
+        for (int x = 0; x < SIZE; x++) {
+            if (board[row][x] == num || board[x][col] == num ||
+                board[row - row % SUBGRID_SIZE + x / SUBGRID_SIZE][col - col % SUBGRID_SIZE + x % SUBGRID_SIZE] == num) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static void printBoard(int[][] board) {
+        for (int row = 0; row < SIZE; row++) {
+            for (int col = 0; col < SIZE; col++) {
+                System.out.print(board[row][col] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
